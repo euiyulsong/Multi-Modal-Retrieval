c659678e1ee7a9fa95f31960af2edaa2de189ab7
EuiYul Song <eysong@uw.edu>
2026-09-20 18:26:52 +0900

    Update script to use fixed version of fashioniq

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-python fashioniq_qwen3vl.py \
-    --data-root ./FashionIQ \
-    --categories dress \
-    --image-batch-size 8 \
-    --text-batch-size 32 \
-    --multimodal-batch-size 4
+python fashioniq_qwen3vl_fixed.py \
+  --data-root ./FashionIQ \
+  --categories dress shirt toptee \
+  --image-batch-size 8 \
+  --text-batch-size 32 \
+  --multimodal-batch-size 4
